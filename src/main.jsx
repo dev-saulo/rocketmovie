ea7218b4fde80e87a139968200150dad9d6622d7
Saulo Christian <saullochristian@outlook.com>
2024-05-09 16:09:56 -0300

    Estruturas e estilos

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './pages/styles/global';
+
+import { AuthProvider } from './hooks/auth';
+
+import theme from './pages/styles/theme';
+
+import { Routes } from './routes';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+        
+    </ThemeProvider>
+  </React.StrictMode>,
+);
