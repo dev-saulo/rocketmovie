196b546f0a27b65f5566bf5ee24f03afc11df633
Saulo Christian <saullochristian@outlook.com>
2024-05-13 19:02:05 -0300

    Inserindo as pag profile, signin e signup

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './pages/styles/global';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { AuthProvider } from './hooks/auth';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
 
-import theme from './pages/styles/theme';
+import { AuthProvider } from "./hooks/auth";
 
-import { Routes } from './routes';
+import theme from "./styles/theme";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode >
+import { Routes } from "./routes";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-
       <AuthProvider>
         <Routes />
       </AuthProvider>
-        
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
